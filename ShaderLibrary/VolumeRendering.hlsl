bfa51d6d526fce6265b1362b6e82c0f95b22433b
Unity Technologies <@unity.com>
2018-09-28 00:00:00 +0200

    com.unity.render-pipelines.core@5.0.0-preview ## [5.0.0-preview] - 2018-09-28 ### Changed - XRGraphicConfig has been changed from a read-write control of XRSettings to XRGraphics, a read-only accessor to XRSettings. This improves consistency of XR behavior between the legacy render pipeline and SRP. - XRGraphics members have been renamed to match XRSettings, and XRGraphics has been modified to only contain accessors potentially useful to SRP - You can now have up to 16 additional shadow-casting lights. ### Fixed - LWRP no longer executes shadow passes when there are no visible shadow casters in a Scene. Previously, this made the Scene render as too dark, overall.

--- ShaderLibrary/VolumeRendering.hlsl
+++ ShaderLibrary/VolumeRendering.hlsl
@@ -49,6 +49,48 @@ real3 TransmittanceIntegralHomogeneousMedium(real3 extinction, real intervalLeng
     return rcp(extinction) - rcp(extinction) * exp(-extinction * intervalLength);
 }
 
+// Can be used to scale base extinction and scattering coefficients.
+real ComputeHeightFogMultiplier(real height, real baseHeight, real2 heightExponents)
+{
+    height = max(0, height - baseHeight);
+
+    return exp(-heightExponents.x * height);
+}
+
+// Optical depth between two endpoints.
+real OpticalDepthHeightFog(real baseExtinction, real baseHeight, real2 heightExponents,
+                           real cosZenith, real startHeight, real intervalLength)
+{
+    // Height fog is composed of two slices:
+    // - constant fog below 'baseHeight'   : d = k * t
+    // - exponential fog above 'baseHeight': d = Integrate[k * e^(-a * (h_0 + z * x)) dx, {x, 0, t}]
+
+    real rcpAbsCos = rcp(max(abs(cosZenith), FLT_EPS));
+
+    real endHeight = startHeight + intervalLength * cosZenith;
+    real minHeight = min(startHeight, endHeight);
+    real maxHeight = max(startHeight, endHeight);
+
+    real constantFogDistance = clamp((baseHeight - minHeight) * rcpAbsCos, 0, intervalLength);
+
+    minHeight = max(0, minHeight - baseHeight);
+    maxHeight = max(0, maxHeight - baseHeight);
+
+    real heightFogFactor  = abs(exp(-heightExponents.x * maxHeight) - exp(-heightExponents.x * minHeight));
+         heightFogFactor *= rcpAbsCos * heightExponents.y;
+
+    return baseExtinction * (constantFogDistance + heightFogFactor);
+}
+
+// This version of the function assumes the interval of infinite length.
+real OpticalDepthHeightFog(real baseExtinction, real baseHeight, real2 heightExponents,
+                           real cosZenith, real startHeight)
+{
+    // TODO: optimize.
+    return OpticalDepthHeightFog(baseExtinction, baseHeight, heightExponents,
+                                 cosZenith, startHeight, rcp(FLT_EPS));
+}
+
 real IsotropicPhaseFunction()
 {
     return INV_FOUR_PI;
@@ -64,7 +106,7 @@ real HenyeyGreensteinPhasePartConstant(real anisotropy)
 real HenyeyGreensteinPhasePartVarying(real anisotropy, real cosTheta)
 {
     real g = anisotropy;
-    real f = rsqrt(1 + g * g - 2 * g * cosTheta); // x^(-1/2)
+    real f = rsqrt(saturate(1 + g * g - 2 * g * cosTheta)); // x^(-1/2)
 
     return f * f * f; // x^(-3/2)
 }
@@ -85,10 +127,14 @@ real CornetteShanksPhasePartConstant(real anisotropy)
 real CornetteShanksPhasePartVarying(real anisotropy, real cosTheta)
 {
     real g = anisotropy;
-    real f = rsqrt(1 + g * g - 2 * g * cosTheta); // x^(-1/2)
+    real f = rsqrt(saturate(1 + g * g - 2 * g * cosTheta)); // x^(-1/2)
     real h = (1 + cosTheta * cosTheta);
 
-    return h * (f * f * f); // h * f^(-3/2)
+    // Note that this function is not perfectly isotropic for (g = 0). We force it to be.
+    // TODO: in the future, when (g = 0), specialize the Volumetric Lighting kernel
+    // to not do anything anisotropy-specific. This way we could avoid this test
+    // (along with tons of other overhead and hacks).
+    return (g == 0) ? 1.33333333 : h * (f * f * f); // h * x^(-3/2)
 }
 
 // A better approximation of the Mie phase function.
@@ -104,11 +150,13 @@ real CornetteShanksPhaseFunction(real anisotropy, real cosTheta)
 // Returns the offset from the start of the interval and the weight = (transmittance / pdf).
 // Ref: Monte Carlo Methods for Volumetric Light Transport Simulation, p. 5.
 void ImportanceSampleHomogeneousMedium(real rndVal, real extinction, real intervalLength,
-                                      out real offset, out real weight)
+                                       out real offset, out real weight)
 {
-    // pdf    = extinction * exp(extinction * (intervalLength - t)) / (exp(intervalLength * extinction - 1)
+    // pdf    = extinction * exp(extinction * (intervalLength - t)) / (exp(intervalLength * extinction) - 1)
+    // pdf    = extinction * exp(-extinction * t) / (1 - exp(-extinction * intervalLength))
     // weight = exp(-extinction * t) / pdf
     // weight = (1 - exp(-extinction * intervalLength)) / extinction
+    // weight = OpacityFromOpticalDepth(extinction * intervalLength) / extinction
 
     real x = 1 - exp(-extinction * intervalLength);
     real c = rcp(extinction);
@@ -118,42 +166,115 @@ void ImportanceSampleHomogeneousMedium(real rndVal, real extinction, real interv
 }
 
 // Implements equiangular light sampling.
-// Returns the distance from the origin of the ray, the squared (radial) distance from the light,
+// Returns the distance from the origin of the ray, the squared distance from the light,
 // and the reciprocal of the PDF.
 // Ref: Importance Sampling of Area Lights in Participating Medium.
-void ImportanceSamplePunctualLight(real rndVal, real3 lightPosition,
+void ImportanceSamplePunctualLight(real rndVal, real3 lightPosition, real lightSqRadius,
                                    real3 rayOrigin, real3 rayDirection,
                                    real tMin, real tMax,
-                                   out real dist, out real rSq, out real rcpPdf,
-                                   real minDistSq = FLT_EPS)
+                                   out real t, out real sqDist, out real rcpPdf)
 {
-    real3 originToLight       = lightPosition - rayOrigin;
-    real  originToLightProj   = dot(originToLight, rayDirection);
-    real  originToLightDistSq = dot(originToLight, originToLight);
-    real  rayToLightDistSq    = max(originToLightDistSq - originToLightProj * originToLightProj, minDistSq);
+    real3 originToLight         = lightPosition - rayOrigin;
+    real  originToLightProjDist = dot(originToLight, rayDirection);
+    real  originToLightSqDist   = dot(originToLight, originToLight);
+    real  rayToLightSqDist      = originToLightSqDist - originToLightProjDist * originToLightProjDist;
 
-    real a    = tMin - originToLightProj;
-    real b    = tMax - originToLightProj;
-    real dSq  = rayToLightDistSq;
-    real dRcp = rsqrt(dSq);
-    real d    = dSq * dRcp;
+    // Virtually offset the light to modify the PDF distribution.
+    real sqD  = max(rayToLightSqDist + lightSqRadius, FLT_EPS);
+    real rcpD = rsqrt(sqD);
+    real d    = sqD * rcpD;
+    real a    = tMin - originToLightProjDist;
+    real b    = tMax - originToLightProjDist;
+    real x    = a * rcpD;
+    real y    = b * rcpD;
 
-    // TODO: optimize me. :-(
-    real theta0 = FastATan(a * dRcp);
-    real theta1 = FastATan(b * dRcp);
-    real gamma  = theta1 - theta0;
-    real theta  = lerp(theta0, theta1, rndVal);
-    real t      = d * tan(theta);
+#if 0
+    real theta0   = FastATan(x);
+    real theta1   = FastATan(y);
+    real gamma    = theta1 - theta0;
+    real tanTheta = tan(theta0 + rndVal * gamma);
+#else
+    // Same but faster:
+    // atan(y) - atan(x) = atan((y - x) / (1 + x * y))
+    // tan(atan(x) + z)  = (x * cos(z) + sin(z)) / (cos(z) - x * sin(z))
+    // Both the tangent and the angle  cannot be negative.
+    real tanGamma = abs((y - x) * rcp(max(0, 1 + x * y)));
+    real gamma    = FastATanPos(tanGamma);
+    real z        = rndVal * gamma;
+    real numer    = x * cos(z) + sin(z);
+    real denom    = cos(z) - x * sin(z);
+    real tanTheta = numer * rcp(denom);
+#endif
 
-    dist   = originToLightProj + t;
-    rSq    = dSq + t * t;
-    rcpPdf = gamma * rSq * dRcp;
+    real tRelative = d * tanTheta;
+
+    sqDist = sqD + tRelative * tRelative;
+    rcpPdf = gamma * rcpD * sqDist;
+    t      = originToLightProjDist + tRelative;
+
+    // Remove the virtual light offset to obtain the real geometric distance.
+    sqDist = max(sqDist - lightSqRadius, FLT_EPS);
 }
 
 // Absorption coefficient from Disney: http://blog.selfshadow.com/publications/s2015-shading-course/burley/s2015_pbs_disney_bsdf_notes.pdf
 real3 TransmittanceColorAtDistanceToAbsorption(real3 transmittanceColor, real atDistance)
 {
     return -log(transmittanceColor + FLT_EPS) / max(atDistance, FLT_EPS);
+}
+
+// TODO: it would be good to improve the perf and numerical stability
+// of approximations below by finding a polynomial approximation.
+
+// input = {radiance, opacity}
+// Note that opacity must be less than 1 (not fully opaque).
+real4 LinearizeRGBA(real4 value)
+{
+    // See "Deep Compositing Using Lie Algebras".
+    // log(A) = {OpticalDepthFromOpacity(A.a) / A.a * A.rgb, -OpticalDepthFromOpacity(A.a)}.
+    // We drop redundant negations.
+    real a = value.a;
+    real d = -log(1 - a);
+    real r = (a >= FLT_EPS) ? (d * rcp(a)) : 1; // Prevent numerical explosion
+    return real4(r * value.rgb, d);
+}
+
+// input = {radiance, optical_depth}
+// Note that opacity must be less than 1 (not fully opaque).
+real4 LinearizeRGBD(real4 value)
+{
+    // See "Deep Compositing Using Lie Algebras".
+    // log(A) = {A.a / OpacityFromOpticalDepth(A.a) * A.rgb, -A.a}.
+    // We drop redundant negations.
+    real d = value.a;
+    real a = 1 - exp(-d);
+    real r = (a >= FLT_EPS) ? (d * rcp(a)) : 1; // Prevent numerical explosion
+    return real4(r * value.rgb, d);
+}
+
+// output = {radiance, opacity}
+// Note that opacity must be less than 1 (not fully opaque).
+real4 DelinearizeRGBA(real4 value)
+{
+    // See "Deep Compositing Using Lie Algebras".
+    // exp(B) = {OpacityFromOpticalDepth(-B.a) / -B.a * B.rgb, OpacityFromOpticalDepth(-B.a)}.
+    // We drop redundant negations.
+    real d = value.a;
+    real a = 1 - exp(-d);
+    real i = (a >= FLT_EPS) ? (a * rcp(d)) : 1; // Prevent numerical explosion
+    return real4(i * value.rgb, a);
+}
+
+// input = {radiance, optical_depth}
+// Note that opacity must be less than 1 (not fully opaque).
+real4 DelinearizeRGBD(real4 value)
+{
+    // See "Deep Compositing Using Lie Algebras".
+    // exp(B) = {OpacityFromOpticalDepth(-B.a) / -B.a * B.rgb, -B.a}.
+    // We drop redundant negations.
+    real d = value.a;
+    real a = 1 - exp(-d);
+    real i = (a >= FLT_EPS) ? (a * rcp(d)) : 1; // Prevent numerical explosion
+    return real4(i * value.rgb, d);
 }
 
 #endif // UNITY_VOLUME_RENDERING_INCLUDED
